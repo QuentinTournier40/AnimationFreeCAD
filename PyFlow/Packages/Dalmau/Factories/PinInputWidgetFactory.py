8f989ae108c305eba181f90af0aaa199e38adc6c
Quentin TOURNIER <qtournier@iutbayone.univ-pau.fr>
2022-01-29 15:42:42 +0100

    Ajout de la VectorPin sans son implementation dans les nodes

--- PyFlow/Packages/Dalmau/Factories/PinInputWidgetFactory.py
+++ PyFlow/Packages/Dalmau/Factories/PinInputWidgetFactory.py
@@ -1,7 +1,13 @@
 from PyFlow.Core.Common import *
 from Qt import QtCore
-from Qt.QtWidgets import QCheckBox
+from Qt.QtWidgets import *
+from PyFlow.Core.Common import *
 from PyFlow.UI.Widgets.InputWidgets import *
+from PyFlow.UI.Widgets.QtSliders import *
+import FreeCAD
+
+FLOAT_SINGLE_STEP = 0.01
+FLOAT_DECIMALS = 5
 
 
 class DemoInputWidget(InputWidgetSingle):
@@ -22,7 +28,63 @@ class DemoInputWidget(InputWidgetSingle):
         else:
             self.cb.setCheckState(QtCore.Qt.Unchecked)
 
+class VectorInputWidget(InputWidgetRaw):
+    def __init__(self, **kwds):
+        super(VectorInputWidget, self).__init__(**kwds)
+        self.setLayout(QtWidgets.QGridLayout())
+        self.x = valueBox(None, "float", True)
+        self.y = valueBox(None, "float", True)
+        self.z = valueBox(None, "float", True)
+        self.labelX = QLabel("X :")
+        self.labelY = QLabel("Y :")
+        self.labelZ = QLabel("Z :")
+
+        self.layout().addWidget(self.labelX, 1, 1)
+        self.layout().addWidget(self.x, 1, 2)
+        self.layout().addWidget(self.labelY, 2, 1)
+        self.layout().addWidget(self.y, 2, 2)
+        self.layout().addWidget(self.labelZ, 3, 1)
+        self.layout().addWidget(self.z, 3, 2)
+
+        self.layout().setColumnStretch(2, 1)
+
+        self.x.valueChanged.connect(self._onDataChangedX)
+        self.y.valueChanged.connect(self._onDataChangedY)
+        self.z.valueChanged.connect(self._onDataChangedZ)
+
+    def blockWidgetSignals(self, bLocked):
+        try:
+            for i in [self.x, self.y, self.z]:
+                i.blockSignals(bLocked)
+        except:
+            pass
+
+    def asDataTypeClass(self):
+        return FreeCAD.Vector([self.x.value(), self.y.value(), self.z.value()])
+
+    def _onDataChangedX(self, val):
+        tmp = self.asDataTypeClass()
+        tmp.x = val
+        self.dataSetCallback(tmp)
+
+    def _onDataChangedY(self, val):
+        tmp = self.asDataTypeClass()
+        tmp.y = val
+        self.dataSetCallback(tmp)
+
+    def _onDataChangedZ(self, val):
+        tmp = self.asDataTypeClass()
+        tmp.z = val
+        self.dataSetCallback(tmp)
+
+    def setWidgetValue(self, val):
+        self.x.setValue(val.x)
+        self.y.setValue(val.y)
+        self.z.setValue(val.z)
 
 def getInputWidget(dataType, dataSetter, defaultValue, widgetVariant=DEFAULT_WIDGET_VARIANT, **kwds):
     if dataType == 'DemoPin':
         return DemoInputWidget(dataSetCallback=dataSetter, defaultValue=defaultValue, **kwds)
+    if dataType == 'VectorPin':
+        return VectorInputWidget(dataSetCallback=dataSetter, defaultValue=defaultValue, **kwds)
+
--- PyFlow/Packages/Dalmau/Pins/VectorPin.py
+++ PyFlow/Packages/Dalmau/Pins/VectorPin.py
@@ -1,38 +1,67 @@
 from PyFlow.Core import PinBase
 from PyFlow.Core.Common import *
-import FreeCAD
+from FreeCAD import Vector
 
-class FakeTypeVTFJO(object):
-    """docstring for FakeTypeVTFJO"""
-    def __init__(self, value=None):
-        super(FakeTypeVTFJO, self).__init__()
-        self.value = value
+import json
+
+class VectorEncoder(json.JSONEncoder):
+    def default(self, vecteur):
+        if isinstance(vecteur, Vector):
+            return {Vector.__name__: [vecteur.x,vecteur.y,vecteur.z]}
+        json.JSONEncoder.default(self, vecteur)
+
+class VectorDecoder(json.JSONDecoder):
+    def __init__(self, *args, **kwargs):
+        super(VectorDecoder, self).__init__(object_hook=self.object_hook, *args, **kwargs)
+
+    def object_hook(self, vecteurDict):
+        return Vector(vecteurDict[Vector.__name__])
+
+def setDataG(self,data):
+    super(self.__class__, self).setData(data)
+    try:
+        if  self.direction == PinDirection.Input and not self.hasConnections():
+            self.owningNode().compute()
+    except:
+        pass
 
 class VectorPin(PinBase):
-    def __init__(self, name, owningNode, direction):
-        super().__init__(name, owningNode, direction)
-        self.setDefaultValue(FreeCAD.Vector(0,0,0))
-    
+    def __init__(self, name, parent, direction, **kwargs):
+        super(VectorPin, self).__init__(name, parent, direction, **kwargs)
+        self.setDefaultValue(Vector(7,2,3))
+
     @staticmethod
     def IsValuePin():
         return True
-    
+
     @staticmethod
     def supportedDataTypes():
-        return ('IntPin', 'FloatPin',)
-    
+        return ('VectorPin')
+
     @staticmethod
     def color():
-        return (65, 0, 255, 255)
+        return (200, 200, 50, 255)
 
     @staticmethod
     def pinDataTypeHint():
-        return 'IntPin', 'FloatPin', ''
+        return 'VectorPin', Vector()
+
+    @staticmethod
+    def jsonEncoderClass():
+        return VectorEncoder
+
+    @staticmethod
+    def jsonDecoderClass():
+        return VectorDecoder
 
     @staticmethod
     def internalDataStructure():
-        return FakeTypeVTFJO
+        return Vector
 
     @staticmethod
     def processData(data):
-        return DemoPin.internalDataStructure()(data)
+        return VectorPin.internalDataStructure()(data)
+
+    def setData(self, data):
+        setDataG(self, data)
+
