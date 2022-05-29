a088a43354190d491809043ef8c83cf352792877
Christopher West <cwest@thedigitaledge.co.uk>
2022-05-29 11:43:30 +0100

    Embbeded the PyFlow UI into the FreeCAD UI
    Includes icon fix from chennes

--- InitGui.py
+++ InitGui.py
@@ -2,24 +2,50 @@ import os
 import FreeCAD as App
 import FreeCADGui as Gui
 
+from pivy import coin
+
+
 class AnimationFreeCAD(Workbench):
     def __init__(self):
-        path = os.getcwd()
-
         self.__class__.MenuText = "AnimationFreeCAD"
         self.__class__.ToolTip = "Faire des animations"
-        self.__class__.Icon = os.path.join(App.getResourceDir(),"../"
+        self.__class__.Icon = os.path.join(App.getUserAppDataDir(),
                                            "Mod", "AnimationFreeCAD",
                                            "icons",
                                            "clapCinema.svg")
 
-    def Initialize(self):
+    def Initialize_Old(self):
         """This function is executed when FreeCAD starts"""
         import pyflow_start
-        from PySide import QtCore, QtGui
+        from PySide2 import QtCore, QtGui
         import PyFlow # import here all the needed files that create your FreeCAD commands
 
         self.list = ["OpenPyFlowCmd"] # A list of command names created in the line above
         self.appendToolbar("open", self.list)
+
+    def Initialize(self):
+        """This function is executed when FreeCAD starts"""
+        from PyFlow.App import PyFlow
+        self.instance = PyFlow.instance(None, "freecad")
+
+    def Activated(self):
+        """This function is executed when the workbench is activated"""
+        from PySide2 import QtWidgets
+        mw = Gui.getMainWindow()
+        mdiArea = mw.findChild(QtWidgets.QMdiArea)
+        mdiArea.addSubWindow(self.instance)
+        self.instance.show()
+
+    def Deactivated(self):
+        """This function is executed when the workbench is deactivated"""
+        return
+
+    def ContextMenu(self, recipient):
+        """This function is executed whenever the user right-clicks on screen"""
+        pass
+
+    def GetClassName(self):
+        """ This function is mandatory if this is a full Python workbench"""
+        return "Gui::PythonWorkbench"
 
 Gui.addWorkbench(AnimationFreeCAD())
